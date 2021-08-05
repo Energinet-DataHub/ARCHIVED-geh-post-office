3a3bad1340fb3b789ecd1ada47735663ad5febce
Alexei Mihalchuk <AIU@energinet.dk>
2021-08-05 13:04:44 +0200

    #118 #119 Repository for DataAvailable

---
+++ source/Energinet.DataHub.PostOffice.Domain/Repositories/IDataAvailableRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+
+namespace Energinet.DataHub.PostOffice.Domain.Repositories
+{
+    /// <summary>
+    /// Repository for DataAvailable aggregates.
+    /// </summary>
+    public interface IDataAvailableRepository
+    {
+        /// <summary>
+        /// Get UUIDs from Cosmos database
+        /// </summary>
+        /// <param name="recipient"></param>
+        /// <returns>A collection with all UUIDs for the specified recipient</returns>
+        public Task<RequestData> GetDataAvailableUuidsAsync(string recipient);
+
+        /// <summary>
+        /// Save a document.
+        /// </summary>
+        /// <param name="document">The document to save.</param>
+        Task<bool> SaveDocumentAsync(DataAvailable document);
+    }
+}
