2b95b2914d06fdb1a2ad36637026d44804a6b3e3
Alexei Mihalchuk <AIU@energinet.dk>
2021-09-28 12:58:22 +0200

    Singleton registration

--- source/Energinet.DataHub.PostOffice.Common/ApplicationServiceRegistration.cs
+++ source/Energinet.DataHub.PostOffice.Common/ApplicationServiceRegistration.cs
@@ -32,22 +32,7 @@ namespace Energinet.DataHub.PostOffice.Common
             container.Register<IDataAvailableNotificationParser, DataAvailableNotificationParser>(Lifestyle.Singleton);
             container.Register<IRequestBundleParser, RequestBundleParser>(Lifestyle.Singleton);
             container.Register<IResponseBundleParser, ResponseBundleParser>(Lifestyle.Singleton);
-
             container.Register<IDataBundleRequestSender, DataBundleRequestSender>(Lifestyle.Singleton);
-
-            // TODO: This should not be scoped. We need to change this to Singleton, but currently there is a limitation
-            // in SimpleInjector when using IAsyncDisposable.
-            //container.Register<IDataBundleRequestSender>(
-            //    () =>
-            //    {
-            //        var dataBundleRequestSender = new DataBundleRequestSender(new RequestBundleParser(), new ResponseBundleParser(), container.GetInstance<IServiceBusClientFactory>());
-            //        dataBundleRequestSender.OnDispose = () =>
-            //        {
-            //            new object().GetHashCode();
-            //        };
-            //        return dataBundleRequestSender;
-            //    },
-            //    Lifestyle.Singleton);
         }
     }
 }
