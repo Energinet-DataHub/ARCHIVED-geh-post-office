b74bc954384b76203eda0db9bab599577dc1f35b
Michael Majgaard <52096428+michaelmajgaard@users.noreply.github.com>
2021-10-08 12:54:31 +0200

    M  source/Energinet.DataHub.PostOffice.IntegrationTests/Common/MockedServiceBusClient.cs

--- source/Energinet.DataHub.PostOffice.IntegrationTests/Common/MockedServiceBusClient.cs
+++ source/Energinet.DataHub.PostOffice.IntegrationTests/Common/MockedServiceBusClient.cs
@@ -17,8 +17,8 @@ using System.Collections.Concurrent;
 using System.Threading;
 using System.Threading.Tasks;
 using Azure.Messaging.ServiceBus;
-using Google.Protobuf;
 using Energinet.DataHub.MessageHub.Client.Protobuf;
+using Google.Protobuf;
 
 namespace Energinet.DataHub.PostOffice.IntegrationTests.Common
 {
