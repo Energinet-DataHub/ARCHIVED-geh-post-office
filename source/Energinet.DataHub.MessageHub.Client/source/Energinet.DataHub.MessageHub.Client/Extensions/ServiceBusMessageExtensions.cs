6dacbcbcfcdb156718064fafd0ce0a00d71c3814
Jesper Justesen <juj@energinet.dk>
2021-10-09 11:43:08 +0200

    Changes after code review

--- source/Energinet.DataHub.MessageHub.Client/source/Energinet.DataHub.MessageHub.Client/Extensions/ServiceBusMessageExtensions.cs
+++ source/Energinet.DataHub.MessageHub.Client/source/Energinet.DataHub.MessageHub.Client/Extensions/ServiceBusMessageExtensions.cs
@@ -1,16 +1,16 @@
-﻿// // Copyright 2020 Energinet DataHub A/S
-// //
-// // Licensed under the Apache License, Version 2.0 (the "License2");
-// // you may not use this file except in compliance with the License.
-// // You may obtain a copy of the License at
-// //
-// //     http://www.apache.org/licenses/LICENSE-2.0
-// //
-// // Unless required by applicable law or agreed to in writing, software
-// // distributed under the License is distributed on an "AS IS" BASIS,
-// // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// // See the License for the specific language governing permissions and
-// // limitations under the License.
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 using System;
 using Azure.Messaging.ServiceBus;
@@ -20,7 +20,7 @@ using static System.Guid;
 
 namespace Energinet.DataHub.MessageHub.Client.Extensions
 {
-    public static class ServiceBusMessageExtensions
+    internal static class ServiceBusMessageExtensions
     {
         public static ServiceBusMessage AddDequeueIntegrationEvents(this ServiceBusMessage serviceBusMessage)
         {
