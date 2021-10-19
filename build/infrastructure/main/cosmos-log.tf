310f37ad595a65b379b641651bf4c0cb3df9dd06
KarstenM <xnetsrak@gmail.com>
2021-10-19 17:38:47 +0200

    Added changes to TF cosmos log setup

--- build/infrastructure/main/cosmos-log.tf
+++ build/infrastructure/main/cosmos-log.tf
@@ -11,37 +11,16 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-resource "azurerm_cosmosdb_account" "log" {
-  name                = "cosmos-log-${var.project}-${var.organisation}-${var.environment}"
-  resource_group_name = data.azurerm_resource_group.postoffice.name
-  location            = data.azurerm_resource_group.postoffice.location
-  offer_type          = "Standard"
-  kind                = "GlobalDocumentDB"
-  # To enable global failover change to true and uncomment second geo_location
-  enable_automatic_failover = false
-
-  consistency_policy {
-    consistency_level = "Session"
-  }
-  
-  geo_location {
-    location          = data.azurerm_resource_group.postoffice.location
-    failover_priority = 0
-  }
-
-  tags                = data.azurerm_resource_group.postoffice.tags
-}
-
-resource "azurerm_cosmosdb_sql_database" "db" {
+resource "azurerm_cosmosdb_sql_database" "log_db" {
   name                = "Log"
   resource_group_name = data.azurerm_resource_group.postoffice.name
   account_name        = azurerm_cosmosdb_account.post_office.name
 }
 
-resource "azurerm_cosmosdb_sql_container" "collection_dataavailable" {
+resource "azurerm_cosmosdb_sql_container" "collection_logs" {
   name                = "Logs"
   resource_group_name = var.resource_group_name
   account_name        = azurerm_cosmosdb_account.post_office.name
-  database_name       = azurerm_cosmosdb_sql_database.db.name
+  database_name       = azurerm_cosmosdb_sql_database.log_db
   partition_key_path  = "/marketOperator"
 }
