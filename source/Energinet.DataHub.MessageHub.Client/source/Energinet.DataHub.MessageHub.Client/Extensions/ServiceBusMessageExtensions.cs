a2a41abe8920075612e63f1b3f7bdfe4c919bf9c
Michael Majgaard <52096428+michaelmajgaard@users.noreply.github.com>
2021-10-25 13:46:02 +0200

    M  source/Energinet.DataHub.MessageHub.Client/source/Energinet.DataHub.MessageHub.Client/Extensions/ServiceBusMessageExtensions.cs

--- source/Energinet.DataHub.MessageHub.Client/source/Energinet.DataHub.MessageHub.Client/Extensions/ServiceBusMessageExtensions.cs
+++ source/Energinet.DataHub.MessageHub.Client/source/Energinet.DataHub.MessageHub.Client/Extensions/ServiceBusMessageExtensions.cs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System;
 using Azure.Messaging.ServiceBus;
 using Energinet.DataHub.MessageHub.Model.IntegrationEvents;
 using static System.DateTimeOffset;
@@ -44,9 +43,6 @@ namespace Energinet.DataHub.MessageHub.Client.Extensions
             IntegrationEventsMessageType messageType,
             string eventIdentification)
         {
-            if (serviceBusMessage is null)
-                throw new ArgumentNullException(nameof(serviceBusMessage));
-
             serviceBusMessage.ApplicationProperties.Add("OperationTimestamp", UtcNow);
             serviceBusMessage.ApplicationProperties.Add("OperationCorrelationId", operationCorrelationId);
             serviceBusMessage.ApplicationProperties.Add("MessageVersion", 1);
