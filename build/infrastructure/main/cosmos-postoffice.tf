2c84e1ffb644c48c75b5ff0cb76fb3a8365f39af
Alexei Mihalchuk <AIU@energinet.dk>
2022-05-10 16:18:02 +0200

    Added Actor table to Cosmos.

--- build/infrastructure/main/cosmos-postoffice.tf
+++ build/infrastructure/main/cosmos-postoffice.tf
@@ -46,6 +46,14 @@ resource "azurerm_cosmosdb_sql_database" "db" {
   account_name        = azurerm_cosmosdb_account.post_office.name
 }
 
+resource "azurerm_cosmosdb_sql_container" "collection_actor" {
+  name                = "actor"
+  resource_group_name = var.resource_group_name
+  account_name        = azurerm_cosmosdb_account.post_office.name
+  database_name       = azurerm_cosmosdb_sql_database.db.name
+  partition_key_path  = "/partitionKey"
+}
+
 resource "azurerm_cosmosdb_sql_container" "collection_catalog" {
   name                = "catalog"
   resource_group_name = var.resource_group_name
